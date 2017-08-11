7111ce2b0da587406db336b24c160c44b3fcdf5d
eranight <abelnigt@gmail.com>
2017-08-11 13:46:09 +0300

    VS

--- SafeLog.cpp
+++ SafeLog.cpp
@@ -1,119 +1,118 @@
-#include "safe_log.h"
+#include "SafeLog.h"
 
 #include <thread>
 #include <string>
 #include <map>
 #include <chrono>
 #include <ctime>
-//#include <iostream>
 
 using namespace safelog;
 using namespace std::chrono;
 
 static std::map<SafeLog::MessageType, std::string> labelNameMap =
 {
-  { SafeLog::MessageType::ERROR, "[Error]: "},
-  { SafeLog::MessageType::WARNING, "[Warning]: " },
-  { SafeLog::MessageType::DEBUG, "[Debug]: " },
+	{ SafeLog::MessageType::ERROR, "[Error]: " },
+	{ SafeLog::MessageType::WARNING, "[Warning]: " },
+	{ SafeLog::MessageType::DEBUG, "[Debug]: " },
 };
 
 SafeLog::InnerSafeLog::InnerSafeLog() :
-  isRunning_(false)
+	isRunning_(false)
 {
 
 }
 
 SafeLog::InnerSafeLog::InnerSafeLog(const std::string & filePath) :
-  isRunning_(false)
+	isRunning_(false)
 {
-  reset(filePath);
+	reset(filePath);
 }
 
 void SafeLog::InnerSafeLog::reset(const std::string & filePath)
 {
-  if (logFile_.is_open())
-    logFile_.close();
-  logFile_.open(filePath, std::ios::app);
-  if (logFile_.is_open())
-  {
-    isRunning_ = true;
-    std::thread thread(std::bind(&InnerSafeLog::mainLoop, this));
-    thread.detach();
-  }
+	if (logFile_.is_open())
+		logFile_.close();
+	logFile_.open(filePath, std::ios::app);
+	if (logFile_.is_open())
+	{
+		isRunning_ = true;
+		std::thread thread(std::bind(&InnerSafeLog::mainLoop, this));
+		thread.detach();
+	}
 }
 
 void SafeLog::InnerSafeLog::pushMessage(const std::string & message)
 {
-  std::unique_lock<std::mutex> lock(mutex_);
-  messageQueue_.push(message);
-  hasMessage_.notify_one();
+	std::unique_lock<std::mutex> lock(mutex_);
+	messageQueue_.push(message);
+	hasMessage_.notify_one();
 }
 
 void SafeLog::InnerSafeLog::mainLoop()
 {
-  while (isRunning_)
-  {
-    std::unique_lock<std::mutex> lock(mutex_);
-    hasMessage_.wait(lock);
-    if (!isRunning_) break;
-    safePop();
-  }
-  messageQueue_.push("End of session");
-  while (!messageQueue_.empty())
-      safePop();
-  logFile_.close();
-  delete this;
+	while (isRunning_)
+	{
+		std::unique_lock<std::mutex> lock(mutex_);
+		hasMessage_.wait(lock);
+		if (!isRunning_) break;
+		safePop();
+	}
+	messageQueue_.push("End of session");
+	while (!messageQueue_.empty())
+		safePop();
+	logFile_.close();
+	delete this;
 }
 
 void SafeLog::InnerSafeLog::safePop() {
-  logFile_ << messageQueue_.front();
-  logFile_.flush();
-  messageQueue_.pop();
+	logFile_ << messageQueue_.front();
+	logFile_.flush();
+	messageQueue_.pop();
 }
 
 ILoginWritter & SafeLog::InnerLoginWritter::operator<<(const std::string & message) {
-  sl_.innerLog_->pushMessage(label_ + message + "\n\n");
-  return *this;
+	sl_.innerLog_->pushMessage(label_ + message + "\n\n");
+	return *this;
 }
 
 SafeLog::SafeLog() :
-  logginWritter_(*this)
+	logginWritter_(*this)
 {
-  innerLog_ = new InnerSafeLog();
+	innerLog_ = new InnerSafeLog();
 }
 
 SafeLog::SafeLog(const std::string & filePath) :
-  logginWritter_(*this)
+	logginWritter_(*this)
 {
-  innerLog_ = new InnerSafeLog(filePath);
+	innerLog_ = new InnerSafeLog(filePath);
 }
 
 SafeLog::~SafeLog()
 {
-  innerLog_->stop();
+	innerLog_->stop();
 }
 
 void SafeLog::reset(const std::string & filePath)
 {
-  innerLog_->reset(filePath);
+	innerLog_->reset(filePath);
 }
 
 /*SafeLog & SafeLog::operator<<(const std::string & message)
 {
-  innerLog_->pushMessage(message + "\n\n");
-  return *this;
+innerLog_->pushMessage(message + "\n\n");
+return *this;
 }*/
 
 ILoginWritter & SafeLog::operator()(const MessageType & messageType)
 {
-  logginWritter_.SetLabel("[" + getTimeAsString() + "]" + labelNameMap[messageType]);
-  return logginWritter_;
+	logginWritter_.SetLabel("[" + getTimeAsString() + "]" + labelNameMap[messageType]);
+	return logginWritter_;
 }
 
 std::string SafeLog::getTimeAsString()
 {
-  system_clock::time_point p = system_clock::now();
-  std::time_t t = system_clock::to_time_t(p);
-  std::string timestr = std::ctime(&t);
-  return std::string(timestr.begin(), timestr.end() - 1);
+	system_clock::time_point p = system_clock::now();
+	std::time_t t = system_clock::to_time_t(p);
+	std::string timestr = std::ctime(&t);
+	return std::string(timestr.begin(), timestr.end() - 1);
 }
--- SafeLog.h
+++ SafeLog.h
@@ -1,19 +1,19 @@
 /**********************************
 Creation:
-  SafeLog logger() - creation without running
-  SafeLog logger("/logs/connection.log") - creation with running
+SafeLog logger() - creation without running
+SafeLog logger("/logs/connection.log") - creation with running
 
 Reset:
-  logger.reset("/logs/otherlog.log") - use this method when you need to change a logfile or to running logger if you have used default constructor
+logger.reset("/logs/otherlog.log") - use this method when you need to change a logfile or to running logger if you have used default constructor
 
 Write an error message:
-  logger(SL_ERROR) << "some error"
+logger(SL_ERROR) << "some error"
 
 Write a warning message:
-  logger(SL_WARNING) << "some warning message"
+logger(SL_WARNING) << "some warning message"
 
 Write a debug message:
-  logger(SL_DEBUG) << "some debug message"
+logger(SL_DEBUG) << "some debug message"
 
 ***********************************/
 
@@ -38,69 +38,69 @@ Write a debug message:
 
 namespace safelog {
 
-  class ILoginWritter {
-  public:
-    virtual ILoginWritter & operator<<(const std::string & message) = 0;
-  };
+	class ILoginWritter {
+	public:
+		virtual ILoginWritter & operator<<(const std::string & message) = 0;
+	};
 
-  class SafeLog
-  {
-  public:
+	class SafeLog
+	{
+	public:
 
-    enum class MessageType
-    {
-      ERROR,
-      WARNING,
-      DEBUG
-    };
+		enum class MessageType
+		{
+			ERROR,
+			WARNING,
+			DEBUG
+		};
 
-    SafeLog();
-    SafeLog(const std::string & filePath);
-    ~SafeLog();
+		SafeLog();
+		SafeLog(const std::string & filePath);
+		~SafeLog();
 
-    //SafeLog & operator<<(const std::string & message);
-  ILoginWritter & operator()(const MessageType & messageType);
+		//SafeLog & operator<<(const std::string & message);
+		ILoginWritter & operator()(const MessageType & messageType);
 
-    void reset(const std::string & filePath);
+		void reset(const std::string & filePath);
 
-  private:
+	private:
 
-    class InnerSafeLog
-    {
-    public:
-      InnerSafeLog();
-      InnerSafeLog(const std::string & filePath);
+		class InnerSafeLog
+		{
+		public:
+			InnerSafeLog();
+			InnerSafeLog(const std::string & filePath);
 
-      void reset(const std::string & filePath);
-      void pushMessage(const std::string & message);
-      void stop() { isRunning_ = false; hasMessage_.notify_one(); }
+			void reset(const std::string & filePath);
+			void pushMessage(const std::string & message);
+			void stop() { isRunning_ = false; hasMessage_.notify_one(); }
 
-    private:
-      bool isRunning_;
+		private:
+			bool isRunning_;
 
-      std::ofstream logFile_;
-      std::queue<std::string> messageQueue_;
-      std::mutex mutex_;
-      std::condition_variable hasMessage_;
+			std::ofstream logFile_;
+			std::queue<std::string> messageQueue_;
+			std::mutex mutex_;
+			std::condition_variable hasMessage_;
 
-      void mainLoop();
-      void safePop();
-    }  *innerLog_;
+			void mainLoop();
+			void safePop();
+		}  *innerLog_;
 
-  class InnerLoginWritter : public ILoginWritter {
-  public:
-    InnerLoginWritter(SafeLog & sl) : sl_(sl) {}
-    ILoginWritter & operator<<(const std::string & message) override;
-    void SetLabel(const std::string & label) { label_ = label; }
+		class InnerLoginWritter : public ILoginWritter {
+		public:
+			InnerLoginWritter(SafeLog & sl) : sl_(sl) {}
+			ILoginWritter & operator<<(const std::string & message) override;
+			void SetLabel(const std::string & label) { label_ = label; }
 
-  private:
-    SafeLog & sl_;
-    std::string label_;
+		private:
+			SafeLog & sl_;
+			std::string label_;
 
-    friend SafeLog;
-  } logginWritter_;
+			friend SafeLog;
+		} logginWritter_;
 
-    std::string getTimeAsString();
-  };
+		std::string getTimeAsString();
+	};
 
 }
